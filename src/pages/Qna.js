42b49e632e113335afaab53136278fdef8b885a9
bryantang1107 <bryantang10@gmail.com>
2022-04-13 13:17:12 +0800

    Latest Version

--- src/pages/Qna.js
+++ src/pages/Qna.js
@@ -3,15 +3,16 @@ import Question from "../support/Question";
 import { motion } from "framer-motion";
 
 const Qna = () => {
-  return;
-  <motion.div
-    animate={{ opacity: 1 }}
-    initial={{ opacity: 0 }}
-    exit={{ opacity: 0 }}
-    transition={{ duration: 1 }}
-  >
-    <Question></Question>
-  </motion.div>;
+  return (
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <Question></Question>
+    </motion.div>
+  );
 };
 
 export default Qna;
